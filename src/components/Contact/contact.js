d40d188194f09317319b39d230f8b766e04ded43
Jhersvin Villodas Quinto <jhersvinvillodasquinto@gmail.com>
2023-09-29 09:35:40 -0500

    DOT

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -45,7 +45,7 @@ function Contact() {
             </div>
             <div className='contact'>
                 <h1 className='contactPageTitle'>Contact Me</h1>
-                <span className='contactDesc'>Please fill out the form below to discuss any work opportunities</span>
+                <span className='contactDesc'>Please fill out the form below to discuss any work opportunities.</span>
                 <form className='contactForm' ref={form} onSubmit={sendEmail}>
                     <input type='text' className='name' placeholder='Your Name' name='from_name'></input>
                     <input type='email' className='email' placeholder='Your Email' name='from_email'></input>
